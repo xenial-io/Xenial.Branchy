9c31c6d19d75135c8f96f95d893508330cc3cd2d
Manuel Grundner <m.grundner@delegate.at>
2022-03-22 13:32:37 +0100

    feat: branchy now can switch branches and stash

--- Xenial.Branchy/Program.cs
+++ Xenial.Branchy/Program.cs
@@ -21,7 +21,7 @@ try
         .Where(b => !b.StartsWith('*')) //we don't need the current branch
         .ToArray();
 
-    AnsiConsole.MarkupLine($"[grey]You are currently on branch [silver]{branch}[/][/]");
+    AnsiConsole.MarkupLine($"[grey]You are currently on branch [green]{branch.EscapeMarkup()}[/][/]");
     AnsiConsole.MarkupLine($"[grey]Press [silver]Ctrl+C[/] to exit at any time.[/]");
     AnsiConsole.WriteLine();
 
@@ -40,17 +40,45 @@ try
 
     var changes = changesString
         .Split('\n')
-        .Where(b => !string.IsNullOrWhiteSpace(b))
         .Select(b => b.Trim())
+        .Where(b => !string.IsNullOrWhiteSpace(b))
         .ToArray();
 
-    var needStash = !string.IsNullOrEmpty(changesString);
+    var needStash = changes.Length > 0;
     var shouldStash = false;
     if (needStash)
     {
         AnsiConsole.MarkupLine($"[yellow]It seams you have [green]{changes.Length} changes[/] in your working copy.[/]");
         shouldStash = AnsiConsole.Confirm("[yellow]Do you want to automatically stash and restore them?[/]");
     }
+
+    shouldStash = needStash && shouldStash;
+
+    if (shouldStash)
+    {
+        AnsiConsole.MarkupLine($"[grey]Stashing changes...[/]");
+        AnsiConsole.WriteLine();
+        await RunAsync("git", "stash");
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine($"[grey]Stashed changes.[/]");
+    }
+
+    AnsiConsole.MarkupLine($"[grey]Switching to branch [silver]{newBranch.EscapeMarkup()}[/]...[/]");
+    AnsiConsole.WriteLine();
+    await RunAsync("git", $"checkout {newBranch}");
+    AnsiConsole.WriteLine();
+    AnsiConsole.MarkupLine($"[grey]Switched to branch [silver]{newBranch.EscapeMarkup()}[/].[/]");
+
+    if (shouldStash)
+    {
+        AnsiConsole.MarkupLine($"[grey]Popping stash.[/]");
+        AnsiConsole.WriteLine();
+        await RunAsync("git", "stash pop");
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine($"[grey]Popped stash.[/]");
+    }
+
+    AnsiConsole.MarkupLine($"[grey][green]Switched[/] to branch [green]{newBranch.EscapeMarkup()}[/][/]");
 
     return 0;
 }
